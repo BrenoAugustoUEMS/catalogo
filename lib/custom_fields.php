2314569f1dea7cc01b11b78b5c904ed27c1567cb
Breno Augusto <brenoaugusto@uems.br>
2024-12-03 15:52:32 +0000

    Tags Funcionando

--- lib/custom_fields.php
+++ lib/custom_fields.php
@@ -1,18 +1,32 @@
 <?php
 
 /**
- * Busca e formata os campos personalizados dos cursos.
+ * Busca e formata os campos personalizados para um curso.
  *
- * @return array Dados dos campos personalizados.
+ * @param int $courseid O ID do curso.
+ * @return array Campos personalizados do curso, incluindo o target_tags.
  */
-function local_catalogo_get_custom_fields_for_course($course_id) {
-    $handler = \core_customfield\handler::get_handler('core_course', 'course');
-    $data = $handler->get_instance_data($course_id, true);
+function local_catalogo_get_custom_fields_for_course($courseid) {
+    // Obtém todos os campos personalizados do curso.
+    $customfield_handler = \core_customfield\handler::get_handler('core_course', 'course');
+    $customfields_data = $customfield_handler->get_instance_data($courseid, true);
 
-    $fields = [];
-    foreach ($data as $field_data) {
-        $fields[$field_data->get_field()->get('shortname')] = $field_data->get_value();
+    $custom_fields = [];
+
+    foreach ($customfields_data as $data) {
+        $shortname = $data->get_field()->get('shortname'); // Nome curto do campo.
+        $value = $data->get_value(); // Valor do campo.
+
+        // Adiciona o campo ao array de custom_fields.
+        $custom_fields[$shortname] = $value;
     }
 
-    return $fields;
+    // Processa o campo 'target' para extrair tags separadas por vírgula.
+    if (!empty($custom_fields['target'])) {
+        $custom_fields['target_tags'] = array_map('trim', explode(',', $custom_fields['target']));
+    } else {
+        $custom_fields['target_tags'] = []; // Caso não haja valores no campo.
+    }
+
+    return $custom_fields;
 }
