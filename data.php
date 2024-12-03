faf2b3af912990252a87f64427fbfff1c90f292d
Breno Augusto <101306233+BrenoAugustoUEMS@users.noreply.github.com>
2024-12-03 00:27:44 +0000

    Ajustes no CSS do card de edital

--- data.php
+++ data.php
@@ -2,30 +2,30 @@
 // Dados de mock para testar o renderer.
 return [
     (object)[
-        'id' => 1,
-        'fullname' => 'Edital de Cultura 2024',
-        'imageurl' => 'https://placecats.com/millie_neo/300/200',
-        'organization' => 'Fundação Nacional de Artes',
-        'tags' => ['Cultura', 'Artes'],
-        'deadline' => '03/12/2024',
-        'extended' => false,
-    ],
-    (object)[
         'id' => 2,
         'fullname' => 'Edital de Esportes 2024',
         'imageurl' => 'https://placecats.com/millie/300/150',
-        'organization' => 'Ministério do Esporte',
+        'organization' => 'PROEC',
         'tags' => ['Esportes', 'Educação'],
         'deadline' => '05/12/2024',
         'extended' => true,
     ],
     (object)[
         'id' => 3,
-        'fullname' => 'Edital de Tecnologia 2024',
+        'fullname' => 'Edital PIBIC 2025',
         'imageurl' => 'https://placecats.com/bella/300/200',
-        'organization' => 'Agência Nacional de Tecnologia',
+        'organization' => 'PROPPI',
         'tags' => ['Tecnologia', 'Inovação'],
         'deadline' => '15/12/2024',
+        'extended' => false,
+    ],
+    (object)[
+        'id' => 4,
+        'fullname' => 'Edital PIBEX 2025',
+        'imageurl' => 'https://placecats.com/millie_neo/300/200',
+        'organization' => 'PROEC',
+        'tags' => ['Cultura', 'Extensão', 'Educação'],
+        'deadline' => '03/12/2024',
         'extended' => false,
     ],
 ];
